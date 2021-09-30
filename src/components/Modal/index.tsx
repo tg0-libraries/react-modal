5fa57490905593fefe31500232f4f4d5a94a7588
Tiago Gonçalves <tiaguin180@gmail.com>
2021-09-30 12:17:40 -0300

    feat: Added id to style

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -20,21 +20,35 @@ type ModalProps = {
 		container?: string;
 		content?: string;
 	};
+
+	ids?: {
+		container?: string;
+		content?: string;
+	};
 };
 
 export function Modal({ 
 	children, 
 	isOpen, 
 	styles, 
-	classeNames 
+	classeNames,
+	ids
 }: ModalProps): JSX.Element {
 	return (
 		// Verify if state is true to show modal
 		<>
 			{isOpen && (
 				<>
-					<Container className={classeNames?.container} style={styles?.container}>
-						<Content className={classeNames?.content} style={styles?.content}>
+					<Container 
+						className={classeNames?.container} 
+						id={ids?.container}
+						style={styles?.container}
+					>
+						<Content 
+							className={classeNames?.content} 
+							id={ids?.content}
+							style={styles?.content}
+						>
 							<>
 								{children}
 							</>
