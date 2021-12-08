19aa715a79d05f5e9392bf2fc88674c9ec2331fa
Tiago Gonçalves <tiaguin180@gmail.com>
2021-12-08 16:41:42 -0300

    refactor: Removed excess props and content tag

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,58 +1,30 @@
-import React, { ReactNode } from "react";
-
 import { ModalGlobalStyled } from '../../styles/global';
 
 import { 
 	Container, 
-	Content 
 } from "./styles";
 
 type ModalProps = {
-	children: ReactNode;
+	children?: React.ReactNode;
 	isOpen: boolean;
 
-	styles?: {
-		container?: React.CSSProperties;
-		content?: React.CSSProperties;
-	};
-
-	classeNames?: {
-		container?: string;
-		content?: string;
-	};
-
-	ids?: {
-		container?: string;
-		content?: string;
-	};
+	containerTag?: React.HTMLAttributes<HTMLDivElement>;
 };
 
 export function Modal({ 
 	children, 
-	isOpen, 
-	styles, 
-	classeNames,
-	ids
+	isOpen,
+	containerTag,
 }: ModalProps): JSX.Element {
 	return (
 		// Verify if state is true to show modal
 		<>
 			{isOpen && (
 				<>
-					<Container 
-						className={classeNames?.container} 
-						id={ids?.container}
-						style={styles?.container}
-					>
-						<Content 
-							className={classeNames?.content} 
-							id={ids?.content}
-							style={styles?.content}
-						>
-							<>
-								{children}
-							</>
-						</Content>
+					<Container {...containerTag}>
+						<>
+							{children}
+						</>
 					</Container>
 
 					<ModalGlobalStyled />
